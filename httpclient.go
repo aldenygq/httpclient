82c7c7f4960441add122a958a6a99f8757fa0be5
guoqiang0114 <ygq13572549874@163.com>
2024-03-10 15:20:03 +0800

    add timeout

---
+++ httpclient.go
@@ -0,0 +1,118 @@
+package httpclient
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/url"
+	"time"
+)
+type HttpClient struct {
+	client *http.Client
+}
+
+func NewHttpClient(timeout int) *HttpClient {
+	/*
+	transport := &http.Transport{
+		//Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   2 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		MaxIdleConns:        100,
+		IdleConnTimeout:     90 * time.Second,
+		//TLSHandshakeTimeout: 2 * time.Second,
+	}
+	 */
+	return &HttpClient{client: &http.Client{
+		Timeout:   time.Second * 2,
+		//Transport: transport,
+		},
+	}
+}
+
+func (c *HttpClient) DoReq(method, u string, body interface{},header map[string]string,queryparams map[string]string) (*http.Response, error) {
+	var (
+		req *http.Request
+		err error
+	)
+	req,err = c.NewReqByMethod(method, u,body,queryparams)
+	if err != nil {
+		return nil, err
+	}
+	
+	err = c.SetRequestHeader(req,header)
+	if err != nil {
+		return nil, err
+	}
+	
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil,err
+	}
+	
+	defer resp.Body.Close()
+	
+	return resp, nil
+}
+func (c *HttpClient) NewReqByMethod(method, u string, body interface{},queryparams map[string]string) (*http.Request,error) {
+	var (
+		err error
+		req *http.Request = &http.Request{}
+		b []byte = make([]byte,0)
+	)
+	if body != nil {
+		b, err = json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if method == "GET" || method == "DELETE" {
+		req, err = http.NewRequest(method, u, nil)
+		_,err = c.SetQuery(u,queryparams)
+		if err != nil {
+			return nil,err
+		}
+	} else if  method == "POST" || method == "PUT" {
+		req, err = http.NewRequest(method, u, bytes.NewReader(b))
+	} else {
+		return nil,errors.New("request method invalid")
+	}
+	return req,nil
+}
+func (c *HttpClient) SetRequestHeader(req *http.Request, header map[string]string) error {
+	if header != nil {
+		for k, v := range header {
+			req.Header.Set(k, v)
+		}
+	}
+	return nil
+}
+
+//set query
+func (c *HttpClient) SetQuery(u string, params map[string]string) (string, error) {
+	var q url.Values
+	_u, err := url.Parse(u)
+	if err != nil {
+		return "", err
+	}
+	if params != nil {
+		q = _u.Query()
+		for k, v := range params {
+			q.Set(k, v)
+		}
+	}
+	_u.RawQuery = q.Encode()
+	
+	return _u.String(), nil
+}
+
+//去掉url配置中最后一个‘/’
+func CutLastestSlash(u string) string {
+	if u[len(u)-1] == '/' {
+		u = u[0:(len(u) - 1)]
+	}
+	
+	return u
+}
---
+++ httpclient_test.go
@@ -0,0 +1,26 @@
+package httpclient
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_getUrl(t *testing.T) {
+	var (
+		u string =  "https://oapi.dingtalk.com/robot/send"
+		paramkey string = "access_token"
+		paramvalue string = "ac0066b0a6335d82953934adf6dc32e4d8f539851b7bf788f14e0d744a35e5a1"
+		queryparam map[string]string = make(map[string]string,0)
+	)
+	
+	queryparam[paramkey] = paramvalue
+	client := NewHttpClient(1)
+	
+	url,err := client.SetQuery(u,queryparam)
+	if err != nil {
+		fmt.Printf("url set query param failed:%v\n",err)
+		return
+	}
+	
+	fmt.Printf("url:%v\n",url)
+}
